91c5f61460b646481279985633bda8631d9b7da9
Raymond Zeng <shilei.zeng98@outlook.com>
2023-07-30 12:59:23 -0400

    Initialize Library

---
+++ script.js
@@ -0,0 +1,27 @@
+
+// Array to hold Book Objects
+let myLibrary = [];
+
+// Book object constructor function
+function Book(title, author, genre, isRead){
+  this.title = title
+  this.author = author
+  this.genre = genre
+  this.isRead = isRead
+}
+
+// Takes user input to add Book Object to myLibrary
+function addBookToLibrary(title, author, genre, isRead){
+  const book = new Book(title, author, genre, isRead)
+  myLibrary.unshift(book);
+}
+
+// Displays the Books 
+function displayBooks(myLibrary){
+  
+}
+
+// Add pre-existing books to test on
+addBookToLibrary('Floriography: An Illustrated Guide to the Victorian Language of Flowers', 'Jessica Roux', 'Reference work', false);
+
+displayBooks(myLibrary);
