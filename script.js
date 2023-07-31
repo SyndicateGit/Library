5fdf37ecb3d51fc3be138078c7545cb8fe1a1654
Raymond Zeng <shilei.zeng98@outlook.com>
2023-07-31 17:27:29 -0400

    Added + New Book form function

--- script.js
+++ script.js
@@ -16,9 +16,15 @@ function addBookToLibrary(title, author, genre, isRead){
   myLibrary.unshift(book);
 }
 
+// Add pre-existing books to test on
+addBookToLibrary('Floriography: An Illustrated Guide to the Victorian Language of Flowers', 'Jessica Roux', 'Reference work', true);
+
 
 // Displays the Books 
 function displayBooks(myLibrary){
+  // Clear Library before redisplaying
+  document.querySelector('.book-display-container').innerHTML="";
+
   myLibrary.forEach(element => {
     /*Inserts Book html to book container. Info based on object.*/
     const div = document.createElement('div');
@@ -51,7 +57,68 @@ function displayBooks(myLibrary){
   });
 }
 
-// Add pre-existing books to test on
-addBookToLibrary('Floriography: An Illustrated Guide to the Victorian Language of Flowers', 'Jessica Roux', 'Reference work', true);
+// Displays form
+function openForm(){
+  document.querySelector(".form-container").style.display="flex";
+}
+
+// Closes form
+function closeForm(){
+  document.querySelector(".form-container").style.display="none";
+}
+
+// Default form closed
+closeForm();
+
+function getBookFromInput(){
+  const title = document.getElementById('title').value 
+  const author = document.getElementById('author').value
+  const genre = document.getElementById('genre').value
+  const isRead = document.getElementById('isRead').checked
+  return new Book(title, author, genre, isRead)
+}
+
+function submitForm(e){
+
+  e.preventDefault();
+
+  const newBook = getBookFromInput()
+  
+  // Prevents submission if required fields are empty
+  if(newBook.title == '' || newBook.author ==''|| newBook.genre == ''){
+    alert("Required fields are empty.")
+    return
+  }
+  
+  // Add new book to 
+  addBookToLibrary(newBook.title, newBook.author, newBook.genre, newBook.isRead);
+
+  /*Close form after submitting*/
+  closeForm();
+
+  /**Reset form values */
+  document.getElementById('title').value = "";
+  document.getElementById('author').value = "";
+  document.getElementById('genre').value = "";
+  document.getElementById('isRead').removeAttribute('checked');
+
+  /** Refreshes Library*/
+  displayBooks(myLibrary);
+}
+// Query selectors and Event Listeners
+
+  //+ New Book button opens form
+const newBookBtn = document.querySelector('.button-4')
+newBookBtn.addEventListener("click", openForm)
+
+  // form close button closes form
+  const closeFormBtn = document.querySelector("#cancel")
+  closeFormBtn.addEventListener("click", closeForm);
+
+  // form Add button adds book to library.
+  const addBook = document.querySelector("#add")
+  addBook.addEventListener("click", submitForm);
+
+
 
 displayBooks(myLibrary);
